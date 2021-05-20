4c1f947a5cbf1f2584c7c8d4e866e408cf0114c9
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-20 11:24:51 +0900

    jotaiのget関数を使って派生atom生成のデモ

--- src/App.js
+++ src/App.js
@@ -2,43 +2,19 @@ import { atom, useAtom } from 'jotai';
 
 import './styles.css';
 
-export default function App() {
+const nameAtom = atom('Batman');
+const isNameEmptyAtom = atom((get) => get(nameAtom).length === 0);
+
+export default function HeroName() {
+  const [name, setName] = useAtom(nameAtom);
+  const [isNameEmpty] = useAtom(isNameEmptyAtom);
+
+  const handleChange = (event) => setName(event.target.value);
+
   return (
-    <>
-      <Header />
-      <Main />
-    </>
+    <div>
+      <input type="text" value={name} onChange={handleChange} />
+      <div>Is name empty: {isNameEmpty ? 'Yes' : 'No'}</div>
+    </div>
   );
-}
-
-// ユーザ入力のグローバル状態を保持
-const searchAtom = atom(''); // Global state
-
-function Header() {
-  // ユーザ入力値とその更新関数を返す
-  const [search, setSearch] = useAtom(searchAtom);
-  // イベントハンドラはユーザ入力値を逐一監視si
-  // 更新関数 `setSearch` を更新する
-  const handleChange = (event) => setSearch(event.target.value);
-
-  // イベントハンドラを `<header>` タグに組み込んで返す
-  return (
-    <header>
-      <input
-        autoFocus
-        type="text"
-        value={search}
-        onChange={handleChange} // イベントハンドラ
-        placeholder="Type search query"
-      />
-    </header>
-  );
-}
-
-function Main() {
-  // ユーザ入力値のみ返す
-  const [search] = useAtom(searchAtom);
-
-  // 入力された値を`<main>` タグに組み込んで返す
-  return <main>Search query: "{search}"</main>;
 }
