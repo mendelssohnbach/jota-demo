064ebcc6a19f3f455074f36847d64c40815a19ec
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-20 10:38:31 +0900

    jotaiを使った複数コンポーネントの実装

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import './styles.css';
 
 export const counterAtom = atom(0); // Global state
 
-export default function CounterButton() {
+export default function App() {
+  return (
+    <>
+      <CounterButton />
+      <CurrentCount />
+    </>
+  );
+}
+
+export function CounterButton() {
   const [count, setCount] = useAtom(counterAtom);
 
   const handleClick = () => {
-    setCount((number) => number + 1); // Increment number
+    setCount((number) => number + 1);
   };
 
   return (
@@ -17,4 +26,10 @@ export default function CounterButton() {
       <button onClick={handleClick}>Increment</button>
     </div>
   );
+}
+
+function CurrentCount() {
+  const [count] = useAtom(counterAtom);
+
+  return <div>Current count: {count}</div>;
 }
