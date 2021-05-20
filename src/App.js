961da819617e0d73566890fa8a6041b9241cf781
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-20 11:11:37 +0900

    jotaiを使ったコンポーネント間連携

--- src/App.js
+++ src/App.js
@@ -2,34 +2,43 @@ import { atom, useAtom } from 'jotai';
 
 import './styles.css';
 
-export const counterAtom = atom(0); // Global state
-
 export default function App() {
   return (
     <>
-      <CounterButton />
-      <CurrentCount />
+      <Header />
+      <Main />
     </>
   );
 }
 
-export function CounterButton() {
-  const [count, setCount] = useAtom(counterAtom);
+// ユーザ入力のグローバル状態を保持
+const searchAtom = atom(''); // Global state
 
-  const handleClick = () => {
-    setCount((number) => number + 1);
-  };
+function Header() {
+  // ユーザ入力値とその更新関数を返す
+  const [search, setSearch] = useAtom(searchAtom);
+  // イベントハンドラはユーザ入力値を逐一監視si
+  // 更新関数 `setSearch` を更新する
+  const handleChange = (event) => setSearch(event.target.value);
 
+  // イベントハンドラを `<header>` タグに組み込んで返す
   return (
-    <div>
-      {count}
-      <button onClick={handleClick}>Increment</button>
-    </div>
+    <header>
+      <input
+        autoFocus
+        type="text"
+        value={search}
+        onChange={handleChange} // イベントハンドラ
+        placeholder="Type search query"
+      />
+    </header>
   );
 }
 
-function CurrentCount() {
-  const [count] = useAtom(counterAtom);
+function Main() {
+  // ユーザ入力値のみ返す
+  const [search] = useAtom(searchAtom);
 
-  return <div>Current count: {count}</div>;
+  // 入力された値を`<main>` タグに組み込んで返す
+  return <main>Search query: "{search}"</main>;
 }
